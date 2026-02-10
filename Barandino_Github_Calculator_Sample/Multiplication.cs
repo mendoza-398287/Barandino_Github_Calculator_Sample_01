906dddf77086cd84b414f1e21664518bd93878a1
mendoza.398287 <mendoza.398287@balagtas.sti.edu.ph>
2026-02-10 19:58:50 +0800

    Feat: I added a new file named Multiplication.cs which is a system for multiplying two numbers.

---
+++ Barandino_Github_Calculator_Sample/Multiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barandino_Github_Calculator_Sample
+{
+    internal class Multiplication
+    {
+        public int multiply(int num1, int num2) //gets value from user
+        {
+            return num1 * num2; //returns the multiplied value of int
+        }
+    }
+}
